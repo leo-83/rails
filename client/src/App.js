a019f44971e2509288186574adfa7c4fa20b7d19
Leo Fuchs <leonardo.m.fuchs@gmail.com>
2022-08-29 21:48:00 -0500

    end of lecture

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import Pages from './components/pages/Pages';
+import Navbar from './components/shared/Navbar';
+import Home from './components/shared/Home';
+import About from './components/shared/About';
 
 const App = () => (
-  <>
+  <> 
+    <Navbar />
+    <Home />
+    <About />
     <Pages />
   </>
 )
---
+++ client/src/components/shared/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/pages">Pages</a>
+        </li>
+      </ul>
+    </nav>
+  )
+  
+  export default Navbar;
